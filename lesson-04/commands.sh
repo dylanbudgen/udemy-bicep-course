a8e375f7cc7876b6e749adb82b9f1eac2bc0f8d6
Dylan Budgen <dylanbudgen@gmail.com>
2023-06-21 18:52:44 +0100

    updates

--- lesson-04/commands.sh
+++ lesson-04/commands.sh
@@ -1,6 +1,9 @@
-
 # azure cli
 
+# create resource group
+az group create --location westeurope --name bicep-course
+
+# deploy bicep template
 az deployment group create \
     --subscription udemy-courses \
     --resource-group bicep-course \
--- lesson-04/powershell.ps1
+++ lesson-04/powershell.ps1
@@ -1,6 +1,9 @@
-
 # powershell
 
+# create resource group
+New-AzResourceGroup -Location westeurope -Name bicep-course
+
+# deploy bicep template
 New-AzResourceGroupDeployment `
   -Name deployment `
   -SubscriptionName udemy-courses `
